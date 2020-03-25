0ac335421ef60036e5027545a8f4662ecf2d435f
maxim.malyavko <maxim.malyavko@foxtree.com>
2020-03-25 12:48:47 +0300

    add login component. Login in overlay

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,15 +1,18 @@
-import { isValid } from './utils';
+import { isValid, createModal } from './utils';
 import { Question } from './question';
+import { getAuthForm } from './auth';
 import './style.css';
 
 
 const form = document.getElementById('form'); // метод самый быстрый;
+const modalBtn = document.getElementById('modal-btn'); // метод самый быстрый;
 const input = form.querySelector('#question-input');
 const submitBtn = form.querySelector('#submit');
 
 window.addEventListener('load', Question.renderList)
 
 form.addEventListener("submit", submitFormHandler);
+modalBtn.addEventListener('click', openModal);
 input.addEventListener("input", () => {
   submitBtn.disabled = !isValid(input.value);
 });
@@ -31,4 +34,22 @@ function submitFormHandler(event) {
     });
 
   }
+}
+
+
+function openModal() {
+  createModal("Авторизация", getAuthForm());
+  document
+    .getElementById("auth-form")
+    .addEventListener("submit", authFormHandler, {once: true})    // this event add only first activate
+}
+
+
+function authFormHandler(event){
+  event.preventDefault();
+
+  const email = event.target.querySelector('#email').value;
+  const password = event.target.querySelector('#password').value;
+
+  console.log(email, password)
 }
